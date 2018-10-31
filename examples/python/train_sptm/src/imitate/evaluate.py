3f1c6857ffe5372923f4ddafdcaebaa60c71a506
William Qi <hello@williamqi.com>
2018-10-31 16:18:12 -0400

    add evaluation script for trained models

--- examples/python/train_sptm/src/imitate/evaluate.py
+++ examples/python/train_sptm/src/imitate/evaluate.py
@@ -1,38 +1,69 @@
 import numpy as np
+import vizdoom as vzd
 
+from keras.metrics import binary_accuracy
 from keras.models import load_model
 from setup import setup_game_test
 
 
-wad_path = '../../data/maps/out/gen_4_size_regular_mons_none_steepness_none.wad' # NOQA
-lmp_path = '../../data/exploration/1_0_rec.lmp'
-goals_path = '../../data/exploration/4_2_rec.npy'
-model_path = '../../experiments/trained_models/model.2000.h5'
+def evaluate_scenario(model, wad_path, lmp_path, goals_path):
+    steps = 4200
+    goals = np.load(goals_path)
+    accs = []
 
-goals = np.load(goals_path)
+    # Setup game using WAD and LMP
+    game = setup_game_test(wad_path)
+    game.replay_episode(lmp_path)
 
-game = setup_game_test(wad_path)
+    for i in range(steps - 1):
+        # Get RGBD state from game
+        state = game.get_state()
+        frame = state.screen_buffer
+        depth = state.depth_buffer[:, :, np.newaxis]
+        angle = game.get_game_variable(vzd.GameVariable.ANGLE)
 
-goal_0 = goals[0]
+        # Build RGBD and goal input tensors
+        rgbd = np.concatenate((frame, depth), axis=2)
+        ga = np.append(goals[i], angle)
+
+        rgbd_batch = rgbd[np.newaxis, :, :, :]
+        ga_batch = ga[np.newaxis, :]
+
+        # Get ground true action from game (FORWARD, RIGHT, LEFT)
+        game.advance_action()
+        last_action = game.get_last_action()
+        action_batch = np.array(last_action[7:10])[np.newaxis, :]
+
+        # Compute and record test loss at iteration
+        test_acc = model.test_on_batch([rgbd_batch, ga_batch], action_batch)[1] # NOQA
+
+        if action_batch[0, 0] == 1 and action_batch[0, 1] == 0 and action_batch[0, 2] == 0: # NOQA
+            pass
+        elif action_batch[0, 0] == 0 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
+            pass
+        elif action_batch[0, 0] == 1 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
+            pass
+        else:
+            accs.append(test_acc)
+        # accs.append(test_acc)
+
+    return np.mean(np.array(accs))
+
+
+model_path = '../../experiments/trained_models/model_angle.2500.h5'
+test_wad_ids = [192, 194, 195, 196, 197, 199, 201, 202, 203, 204]
+test_lmp_id = 2
+
 model = load_model(model_path)
+accs = np.zeros(len(test_wad_ids))
 
-for i in range(500):
-    state = game.get_state()
-    frame = state.screen_buffer
-    depth = state.depth_buffer[:, :, np.newaxis]
-    rgbd = np.concatenate((frame, depth), axis=2)
-    rgbd_batch = rgbd[np.newaxis, :, :, :]
+for idx, wad_id in enumerate(test_wad_ids):
+    wad_path = '../../data/maps/out/gen_{}_size_regular_mons_none_steepness_none.wad'.format(wad_id) # NOQA
+    lmp_path = '../../data/exploration/{}_{}_rec.lmp'.format(wad_id, test_lmp_id) # NOQA
+    goals_path = '../../data/exploration/{}_{}_rec.npy'.format(wad_id, test_lmp_id) # NOQA
 
-    goal = goal_0
-    goal_batch = goal[np.newaxis, :]
+    acc = evaluate_scenario(model, wad_path, lmp_path, goals_path)
+    accs[idx] = acc
+    print(acc)
 
-    pred_action = model.predict_on_batch([rgbd_batch, goal_batch]).tolist()[0]
-
-    action = [0.0] * 21
-    action[7] = pred_action[7]
-    action[8] = pred_action[8]
-    action[9] = pred_action[9]
-    print(action)
-    game.make_action(action)
-
-print(goal_0)
+print('TOTAL ACCURACY: ', np.mean(accs))
