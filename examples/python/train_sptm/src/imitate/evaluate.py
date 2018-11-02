45b5a7d00f59ed3270b86d0f417ee8f07b5496d8
William Qi <hello@williamqi.com>
2018-11-02 17:40:32 -0400

    update evaluation code to work with angle history

--- examples/python/train_sptm/src/imitate/evaluate.py
+++ examples/python/train_sptm/src/imitate/evaluate.py
@@ -7,50 +7,79 @@ from setup import setup_game_test
 
 
 def evaluate_scenario(model, wad_path, lmp_path, goals_path):
-    steps = 4200
-    goals = np.load(goals_path)
+    num_steps = 4200
+    history_size = 2
+    rec_goals = np.load(goals_path)
     accs = []
 
     # Setup game using WAD and LMP
     game = setup_game_test(wad_path)
     game.replay_episode(lmp_path)
 
-    for i in range(steps - 1):
-        # Get RGBD state from game
+    # Declare state history matrices
+    frames = np.zeros((history_size + num_steps, 240, 320, 3))
+    depths = np.zeros((history_size + num_steps, 240, 320))
+    angles = np.zeros((history_size + num_steps, 1))
+    goals = np.zeros((history_size + num_steps, 2))
+    # actions = np.zeros((history_size + num_steps, 21))
+
+    for step in range(num_steps - 1):
+        current_time = game.get_episode_time()
+
+        # Get state from game
         state = game.get_state()
         frame = state.screen_buffer
-        depth = state.depth_buffer[:, :, np.newaxis]
+        depth = state.depth_buffer
         angle = game.get_game_variable(vzd.GameVariable.ANGLE)
 
-        # Build RGBD and goal input tensors
-        rgbd = np.concatenate((frame, depth), axis=2)
-        ga = np.append(goals[i], angle)
-
-        rgbd_batch = rgbd[np.newaxis, :, :, :]
-        ga_batch = ga[np.newaxis, :]
+        # Record state
+        data_idx = step + history_size
+        frames[data_idx] = frame
+        depths[data_idx] = depth
+        angles[data_idx] = angle
+        goals[data_idx] = rec_goals[current_time - 1]
 
         # Get ground true action from game (FORWARD, RIGHT, LEFT)
         game.advance_action()
         last_action = game.get_last_action()
         action_batch = np.array(last_action[7:10])[np.newaxis, :]
+        # actions[data_idx] = last_action
+
+        # Build input for network
+        batch_rgbd_all = []
+        batch_ga_all = []
+
+        for j in reversed(range(history_size + 1)):
+            batch_frames = frames[step + j, :, :, :]
+            batch_depths = depths[step + j, :, :][:, :, np.newaxis]
+            batch_rgbd_all.append(batch_frames)
+            batch_rgbd_all.append(batch_depths)
+
+            batch_goals = goals[step + j, :]
+            batch_angles = angles[step + j, :]
+            batch_ga_all.append(batch_goals)
+            batch_ga_all.append(batch_angles)
+
+        batch_rgbd = np.concatenate(batch_rgbd_all, axis=2)[np.newaxis, :, :, :]
+        batch_ga = np.concatenate(batch_ga_all, axis=0)[np.newaxis, :]
 
         # Compute and record test loss at iteration
-        test_acc = model.test_on_batch([rgbd_batch, ga_batch], action_batch)[1] # NOQA
+        test_acc = model.test_on_batch([batch_rgbd, batch_ga], action_batch)[1] # NOQA
 
-        if action_batch[0, 0] == 1 and action_batch[0, 1] == 0 and action_batch[0, 2] == 0: # NOQA
-            pass
-        elif action_batch[0, 0] == 0 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
-            pass
-        elif action_batch[0, 0] == 1 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
-            pass
-        else:
-            accs.append(test_acc)
-        # accs.append(test_acc)
+        # if action_batch[0, 0] == 1 and action_batch[0, 1] == 0 and action_batch[0, 2] == 0: # NOQA
+        #     pass
+        # elif action_batch[0, 0] == 0 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
+        #     pass
+        # elif action_batch[0, 0] == 1 and action_batch[0, 1] == 1 and action_batch[0, 2] == 0: # NOQA
+        #     pass
+        # else:
+        #     accs.append(test_acc)
+        accs.append(test_acc)
 
     return np.mean(np.array(accs))
 
 
-model_path = '../../experiments/trained_models/model_angle.2500.h5'
+model_path = '../../experiments/trained_models/model_angle_history.2500.h5'
 test_wad_ids = [192, 194, 195, 196, 197, 199, 201, 202, 203, 204]
 test_lmp_id = 2
 
